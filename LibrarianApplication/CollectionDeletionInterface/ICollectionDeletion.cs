4c0582a5ab83439f9f13d02a31cd3e7bc956a3e5
Chris Shanley <crs40@students.uwf.edu>
2017-03-23 20:07:13 -0500

    added collection addition and deletion with interfaces. Also added document deletion.

---
+++ LibrarianApplication/CollectionDeletionInterface/ICollectionDeletion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDeletionInterface
+{
+    public interface ICollectionDeletion
+    {
+        String deleteCollection(String deletePath); 
+    }
+}
---
+++ LibrarianApplication/DocumentDeletionImplementation/DocumentDeletion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentDeletionInterface;
+namespace DocumentDeletionImplementation
+{
+    public class DocumentDeletion: IDocumentDeletion
+    {
+       
+        public string deleteDoc(String deletePath)
+        {
+            // Delete a file by using File class static method...
+            if (System.IO.File.Exists(@deletePath))
+            {
+                try
+                {
+                    System.IO.File.Delete(@deletePath);
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+
+            return deletePath;
+        }
+    }
+}
